41c6c84b3cd8d99c80b6a2bf4a700d1532fbe07a
chap <chapambrose@gmail.com>
2013-06-17 18:44:15 -0500

    update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,10 @@ User.create!(
   password_salt: 'salty'
 )
 
-Opro::Oauth::ClientApp.create_with_user_and_name(User.first, 'Artful.ly')
+Opro::Oauth::ClientApp.create(
+  name: "Artful.ly",
+  app_id: "87ef8bc91b2d4173788c43f258f0fd44",
+  app_secret: "1a76a2a53b385c88ac9c7179a9e84ecb",
+  permissions: {},
+  user_id: User.first.id
+)
